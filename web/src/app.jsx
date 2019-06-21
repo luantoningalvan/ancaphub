fcccbcee965e1ca2e79da9993c7a770ce4889e57
Luan Tonin Galvan <ltoningalvan@gmail.com>
2019-06-20 21:41:08 -0300

    Alteração básica de estrutura

--- web/src/app.jsx
+++ web/src/app.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
-import Template from './template/template'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './rootReducer'
+import thunk from 'redux-thunk'
 
-export default props => (
-  <Template>
-    <h1>Home</h1>
-    <p>Esta página está em construção.</p>
-  </Template>
-)
+// Telas
+import Home from './home/home'
+import Livros from './books/books'
+import Podcasts from './podcasts/podcasts'
+import Artigos from './articles/articles'
+import Login from './auth/loginForm'
+import Cadastro from './auth/signup'
 
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/livros" component={Livros} />
+          <Route path="/podcasts" component={Podcasts} />
+          <Route path="/artigos" component={Artigos} />
+          <Route path="/login" component={Login} />
+          <Route path="/cadastro" component={Cadastro} />
+        </Switch>
+      </Router>
+    </Provider>
+  )
+}
