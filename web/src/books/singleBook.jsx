c6ed5f284c8db035770948d8749cf86a789c9fd1
Luan Tonin Galvan <ltoningalvan@gmail.com>
2019-06-29 16:24:36 -0300

    Exibição das Categorias do Livro

--- web/src/books/singleBook.jsx
+++ web/src/books/singleBook.jsx
@@ -5,7 +5,8 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { fetchBook } from './bookActions'
 import { Typography, Box, Grid } from '@material-ui/core';
-
+import Chip from '@material-ui/core/Chip';
+import isEmpty from 'is-empty'
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
@@ -20,36 +21,50 @@ const useStyles = makeStyles(theme => ({
     rightIcon: {
         marginLeft: theme.spacing(1),
     },
+    chip: {
+    marginLeft: theme.spacing(0.5),
+    },
 }));
 
 function SingleBook(props) {
     const { id } = props.match.params;
     useEffect(() => props.fetchBook(id), []);
     const classes = useStyles();
-    const { book } = props;
+    const { _id, title, author, categories, description, cover, downloadOptions } = props.book;
 
     return (
         <Template>
             <Box mb={2}>
-                <Typography variant="h4" component="h2" gutterBottom>{book.title}</Typography>
+                <Typography variant="h4" component="h2" gutterBottom>{title} - {author}</Typography>
+            </Box>
+
+            <Box mb={2}>
+            Categorias: 
+            { !isEmpty(categories) && categories.map(data => (
+                <Chip
+                    key={data._id}
+                    label={data.name}
+                    className={classes.chip}
+                />
+            )) }
             </Box>
 
             <Grid container spacing={3}>
                 <Grid item xs={3}>
-                    <img src={book.cover} alt={`Capa do livro ${book.title}`} style={{width: '100%'}} />
+                    <img src={cover} alt={`Capa do livro ${title}`} style={{width: '100%'}} />
                 </Grid>
 
                 <Grid item xs={6}>
                     <Typography variant="subtitle2" gutterBottom component="h3">Sinopse</Typography>
-                    {book.description}
+                    {description}
                 </Grid>
 
                 <Grid item xs={3}>
                     <Typography variant="subtitle2" gutterBottom component="h3">Opções de Download</Typography>
 
                     <List>
-                        {book.downloadOptions && book.downloadOptions.map(download => (
-                            <ListItem key={`${book._id} ${download.type}`}>
+                        {downloadOptions && downloadOptions.map(download => (
+                            <ListItem key={`${_id} ${download.type}`}>
                                 <ListItemText
                                     primary={download.type.toUpperCase()}
                                 />
