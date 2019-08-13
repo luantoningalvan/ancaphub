ce8e2abe4b7422faec0d760246ac31cf8776d020
Luan Tonin Galvan <ltoningalvan@gmail.com>
2019-08-13 16:47:11 -0300

    Construção da página de gerencimanto de usuários

--- admin/src/rootReducer.js
+++ admin/src/rootReducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux'
 import collectionReducers from './collection/collectionReducers'
+import userReducers from './users/userReducers'
 import categoriesReducer from './components/categories/categoriesReducer'
 import authReducer from './auth/authReducer'
 import errorReducers from './errors/errorReducers'
@@ -7,6 +8,7 @@ import { reducer as toastrReducer } from 'react-redux-toastr'
 
 export default combineReducers({
     collection: collectionReducers,
+    users: userReducers,
     auth: authReducer,
     categories: categoriesReducer,
     errors: errorReducers,
