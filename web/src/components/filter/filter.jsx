7c25e1e02b876ae19912a2cbdb6c8c1e96936b2a
Luan Tonin Galvan <ltoningalvan@gmail.com>
2019-08-17 14:33:55 -0300

    Generalização do texto

--- web/src/components/filter/filter.jsx
+++ web/src/components/filter/filter.jsx
@@ -17,104 +17,104 @@ import { connect } from 'react-redux'
 import { fetchAllCategories } from '../categories/categoriesAction'
 
 const useStyles = makeStyles(theme => ({
-	media: {
-		height: 200,
-	},
-	rightIcon: {
-		marginLeft: theme.spacing(1),
-	},
-	formControl: {
-		minWidth: 200,
-	},
+  media: {
+    height: 200,
+  },
+  rightIcon: {
+    marginLeft: theme.spacing(1),
+  },
+  formControl: {
+    minWidth: 200,
+  },
 }));
 
 
 function Filter(props) {
-	const classes = useStyles();
-	const { category, order, page } = props.filters
-	useEffect(() => props.fetchAction({ category, order, page }), [category, order, page])
-	useEffect(() => props.fetchAllCategories(), [])
+  const classes = useStyles();
+  const { category, order, page } = props.filters
+  useEffect(() => props.fetchAction({ category, order, page }), [category, order, page])
+  useEffect(() => props.fetchAllCategories(), [])
 
 
-	const checkHowManyBooksAreAlreadyLoaded = () => {
-		return page <= parseInt(props.totalItens / props.pageSize) ? page * props.pageSize : props.totalItens
-	}
+  const checkHowManyItensAreAlreadyLoaded = () => {
+    return page <= parseInt(props.totalItens / props.pageSize) ? page * props.pageSize : props.totalItens
+  }
 
-	const beforePageButton = () => {
-		return (
-			<IconButton
-				color="primary"
-				aria-label="Próxima"
-				component="span"
-				size="small"
-				disabled={page == 1}
-				onClick={() => props.selectPage(page - 1)}
-			>
-				<BeforeIcon />
-			</IconButton>
-		)
-	}
+  const beforePageButton = () => {
+    return (
+      <IconButton
+        color="primary"
+        aria-label="Próxima"
+        component="span"
+        size="small"
+        disabled={page == 1}
+        onClick={() => props.selectPage(page - 1)}
+      >
+        <BeforeIcon />
+      </IconButton>
+    )
+  }
 
-	const nextPageButton = () => {
-		return (
-			<IconButton
-				color="primary"
-				aria-label="Próxima"
-				component="span"
-				size="small"
-				disabled = { checkHowManyBooksAreAlreadyLoaded() == props.totalItens }
-				onClick={() => props.selectPage(page + 1)}
-			>
-				<NextIcon />
-			</IconButton>
-		)
-	}
+  const nextPageButton = () => {
+    return (
+      <IconButton
+        color="primary"
+        aria-label="Próxima"
+        component="span"
+        size="small"
+        disabled={checkHowManyItensAreAlreadyLoaded() == props.totalItens}
+        onClick={() => props.selectPage(page + 1)}
+      >
+        <NextIcon />
+      </IconButton>
+    )
+  }
 
-	return (
-		<Box mb={3}>
-			<Paper style={{ paddingRight: '10px', paddingLeft: '10px' }}>
-				<Grid container spacing={2} alignItems="center" justify="space-between">
-					<Grid item xs={3}>
-						<FormControl className={classes.formControl}>
-							<InputLabel htmlFor="age-simple">Filtrar por categoria</InputLabel>
-							<Select
-								value={category}
-								onChange={(e) => props.selectCategory(e.target.value)}
-							>
-								<MenuItem value="">
-									Todas
+  return (
+    <Box mb={3}>
+      <Paper style={{ paddingRight: '10px', paddingLeft: '10px' }}>
+        <Grid container spacing={2} alignItems="center" justify="space-between">
+          <Grid item xs={3}>
+            <FormControl className={classes.formControl}>
+              <InputLabel htmlFor="age-simple">Filtrar por categoria</InputLabel>
+              <Select
+                value={category}
+                onChange={(e) => props.selectCategory(e.target.value)}
+              >
+                <MenuItem value="">
+                  Todas
             					</MenuItem>
-								{!isEmpty(props.categories) && props.categories.allCategories.map(category => (
-									<MenuItem value={category._id} key={category._id}>{category.name}</MenuItem>
-								))}
-							</Select>
-						</FormControl>
-					</Grid>
+                {!isEmpty(props.categories) && props.categories.allCategories.map(category => (
+                  <MenuItem value={category._id} key={category._id}>{category.name}</MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Grid>
 
-					<Grid item xs={3}>
-						<FormControl className={classes.formControl}>
-							<InputLabel>Ordem</InputLabel>
-							<Select
-								value={order}
-								onChange={(e) => props.selectOrder(e.target.value)}
-							>
-								<MenuItem value="asc">Crescente</MenuItem>
-								<MenuItem value="desc">Decrescente</MenuItem>
-							</Select>
-						</FormControl>
-					</Grid>
+          <Grid item xs={3}>
+            <FormControl className={classes.formControl}>
+              <InputLabel>Ordem</InputLabel>
+              <Select
+                value={order}
+                onChange={(e) => props.selectOrder(e.target.value)}
+              >
+                <MenuItem value="asc">Crescente</MenuItem>
+                <MenuItem value="desc">Decrescente</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
 
 
-					<Grid item xs={3}>
-						<span style={{ marginRight: '10px' }}>{`Total de livros: ${checkHowManyBooksAreAlreadyLoaded()} - ${props.totalItens}`}</span>
-						{beforePageButton()}
-						{nextPageButton()}
-					</Grid>
+          <Grid item xs={3}>
+            <span style={{ marginRight: '10px' }}>{`Total de itens: ${checkHowManyItensAreAlreadyLoaded()} - ${props.totalItens}`}</span>
+            {beforePageButton()}
+            {nextPageButton()}
+          </Grid>
 
-				</Grid>
-			</Paper>
-		</Box>
-	)
+        </Grid>
+      </Paper>
+    </Box>
+  )
 }
 const mapStateToProps = (state) => ({ categories: state.categories })
 const mapDispatchToProps = (dispatch) => bindActionCreators({ fetchAllCategories }, dispatch)
