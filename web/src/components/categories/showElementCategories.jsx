4bad83a3c84b1883e4fba92598af5b0fccf83dee
Luan Tonin Galvan <ltoningalvan@gmail.com>
2019-08-26 17:12:51 -0300

    Alerações no design da visualização de artigos

--- web/src/components/categories/showElementCategories.jsx
+++ web/src/components/categories/showElementCategories.jsx
@@ -1,28 +1,17 @@
 import React, { useEffect } from 'react'
 import Chip from '@material-ui/core/Chip'
 import Box from '@material-ui/core/Box'
-import { makeStyles } from '@material-ui/core/styles';
 import isEmpty from 'is-empty'
 
-const useStyles = makeStyles(theme => ({
-  chip: {
-    marginLeft: theme.spacing(1),
-  },
-}));
-
 function Categories(props) {
-  const classes = useStyles()
-
   return (
     <>
       {!isEmpty(props.categories) ? (
         <Box mb={2}>
-          <span style={{ color: 'white' }}>Categorias:</span>
           {props.categories.map(cat => (
             <Chip
               key={cat._id}
               label={cat.name}
-              className={classes.chip}
               variant="outlined"
               color="secondary"
             />
--- web/src/pages/collection/articles/singleArticle.jsx
+++ web/src/pages/collection/articles/singleArticle.jsx
@@ -1,5 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, Fragment } from 'react'
+import { makeStyles } from '@material-ui/core/styles';
 import Template from '../../../template/template'
+import Container from '@material-ui/core/Container'
+import Paper from '@material-ui/core/Paper'
 import Categories from '../../../components/categories/showElementCategories'
 import isEmpty from 'is-empty'
 import parse from 'html-react-parser'
@@ -11,29 +14,64 @@ import { Typography, Box, Grid } from '@material-ui/core'
 function SingleArticle(props) {
   const { id } = props.match.params;
   useEffect(() => props.fetchItem(id), []);
+
   const { title, author, categories, content, cover } = props.article.item;
 
+  const useStyles = makeStyles(theme => ({
+    banner: {
+      position: 'absolute',
+      left: 0,
+      top: 0,
+      background: `url(${cover}) rgba(0,0,0,0.5)`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      width: "100%",
+      height: "300px",
+    },
+    bannelOverlay: {
+      width: '100%',
+      height: '100%',
+      background: 'black',
+      opacity: 0.8
+    },
+    title: {
+      fontWeight: 'bold',
+      color: 'white'
+    },
+    author: {
+      fontWeight: 'light',
+      color: 'white'
+    }
+  }));
+
+  const classes = useStyles();
+
   return (
-    <Template>
+    <Template noPadding>
       {!isEmpty(props.article.item) && props.article.item.type == "article" && (
-        <>
-          <Box>
-            <img src={cover} alt={`Capa do livro ${title}`} />
+        <Fragment>
+          <div className={classes.banner}>
+            <div className={classes.bannelOverlay}></div>
+          </div>
+          <Box mt={8}>
+            <Container style={{ position: 'absolute', width: 'auto' }}>
+              <Box mb={8} display="flex" flexDirection="column" alignItems="center">
+                <Categories categories={categories} />
+                <Typography style={{ textAlign: "center" }} variant="h4" component="h2" className={classes.title}>{title}</Typography>
+                <Typography style={{ textAlign: "center" }} variant="h6" component="h3" className={classes.author}>{author}</Typography>
+              </Box>
+              <Box mb={3}>
+                <Paper>
+                  <Box px={4} py={2}>
+                    {parse(`${content}`)}
+                  </Box>
+                </Paper>
+              </Box>
+            </Container>
           </Box>
-
-          <Box my={2}>
-            <Typography variant="h4" component="h2" gutterBottom>{title} - {author}</Typography>
-          </Box>
-
-          <Categories categories={categories} />
-
-          <Grid container spacing={3}>
-            <Grid item xs={12}>
-              {parse(`${content}`)}
-            </Grid>
-          </Grid>
-        </>
+        </Fragment>
       )}
+
     </Template>
 
   )
