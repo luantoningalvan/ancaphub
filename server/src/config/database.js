7060fdc330e6ceb030c5f136eda0138a739b9409
Luan Tonin Galvan <ltoningalvan@gmail.com>
2019-11-18 17:02:04 -0300

    funciona

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-const databaseUrl = process.env.MONGO_URI || 'mongodb://localhost/ancaphub';
+const databaseUrl = process.env.MONGO_URI;
 module.exports = mongoose.connect(databaseUrl, {
   useNewUrlParser: true,
   useCreateIndex: true,
