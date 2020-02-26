fc6f3eeeb1217fc6d740bf6e6507b40334947213
Luan Tonin Galvan <ltoningalvan@gmail.com>
2020-02-26 16:49:58 -0300

    Adiciona atributo slug e lib para criação automática do mesmo

--- server/src/models/CategoryModel.js
+++ server/src/models/CategoryModel.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const slugify = require('slugify')
 
 const CategorySchema = new Schema({
   name: { type: String, required: true},
+  slug: String,
   children: Array
 })
+
+CategorySchema.pre('save', function() {
+  if (!this.slug) {
+    this.slug = slugify(this.name)
+  }
+});
+
 
 module.exports = mongoose.model('Category', CategorySchema);
