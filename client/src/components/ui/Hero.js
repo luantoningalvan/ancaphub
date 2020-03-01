ea5670f1d262757e412329b6b060cd36c7b08ebe
Luan Tonin Galvan <ltoningalvan@gmail.com>
2020-03-01 14:19:18 -0300

    Correção no tamanho do componente Hero

--- client/src/components/ui/Hero.js
+++ client/src/components/ui/Hero.js
@@ -2,8 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 const HeroWrapper = styled.div`
-  width: 90%;
-  max-width: 1000px;
+  width: 100%;
   border-radius:5px;
   background: ${ props => props.theme.pallete.paper };
   padding: 40px 30px;
