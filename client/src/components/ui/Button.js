dd9cfe15ac0885bef912eb0088e72c565cc77ace
Luan Tonin Galvan <ltoningalvan@gmail.com>
2020-03-01 14:19:00 -0300

    Criação dos componentes Button e Paper

---
+++ client/src/components/ui/Button.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const Button = styled.button`
+    background: ${props => props.color == "secondary" ? props.theme.pallete.secondary : props.theme.pallete.paper};
+    cursor: pointer;
+    color: ${props => props.theme.pallete.text.primary};
+    border-radius: 10px;
+    font-size: .875rem;
+    text-align: center;
+    padding: 15px;
+    transition: background-color .2s ease-in-out,color .2s ease-in-out,border-color .2s ease-in-out,box-shadow .2s ease-in-out;
+    box-shadow: 3px 5px 10px 0 rgba(0,0,0,.2);
+    border: none;
+    outline: none;
+`
+
+export default Button
---
+++ client/src/components/ui/Paper.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const Paper = styled.div`
+    background: ${ props => props.theme.pallete.paper};
+    color: ${props => props.theme.pallete.text.primary};
+    border-radius: 10px;
+    padding: ${props => props.padding && '16px'}
+`
+
+export default Paper
