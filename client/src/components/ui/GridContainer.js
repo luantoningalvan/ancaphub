9f6032c0c5c2671ae8d6083c5cfb8d7e8f1ae51a
Vini Franco <email@vinifranco.com.br>
2020-03-11 11:47:48 -0300

    fix: fixed spacing and split grid components

---
+++ client/src/components/ui/GridContainer.js
@@ -0,0 +1,49 @@
+import React from "react";
+import styled from "styled-components";
+import {
+  propertyDefault,
+  commonPropTypes,
+  generateClassNames,
+  commonStyles
+} from "./Grid";
+
+const GridContainerWrapper = styled.div`
+  display: flex;
+  ${props => commonStyles(props)}
+  box-sizing: border-box;
+  width: 100%;
+`;
+
+GridContainerWrapper.propTypes = commonPropTypes;
+
+const GridContainer = props => {
+  // default prop values:
+  let newProps = {
+    alignContent: propertyDefault(props.alignContent, "stretch"),
+    alignItems: propertyDefault(props.alignItems, "stretch"),
+    flexDirection: propertyDefault(props.flexDirection, "row"),
+    justifyContent: propertyDefault(props.justifyContent, "flex-start"),
+    xs: propertyDefault(props.xs, false),
+    sm: propertyDefault(props.sm, false),
+    md: propertyDefault(props.md, false),
+    lg: propertyDefault(props.lg, false),
+    xl: propertyDefault(props.xl, false),
+    spacing: propertyDefault(props.spacing, 0),
+    flexWrap: propertyDefault(props.flexWrap, "wrap"),
+    zeroMinWidth: propertyDefault(props.zeroMinWidth, false)
+  };
+
+  Object.preventExtensions(newProps);
+
+  return (
+    <GridContainerWrapper
+      {...newProps}
+      style={props.style}
+      className={generateClassNames(newProps)}
+    >
+      {props.children}
+    </GridContainerWrapper>
+  );
+};
+
+export default GridContainer;
