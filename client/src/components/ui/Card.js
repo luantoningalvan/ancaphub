81f4b106ac499fc5226f256b5a6bf4331de33b72
Vini Franco <email@vinifranco.com.br>
2020-03-17 23:01:35 -0300

    feat(ui): added hidden component :rocket:

--- client/src/components/ui/Card.js
+++ client/src/components/ui/Card.js
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 
 const CardBody = styled.div`
-    display:flex;
-    flex-direction:column;
-    width:100%;
-    background: ${ props => props.theme.pallete.paper};
-    color: ${props => props.theme.pallete.text.primary};
-    border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  background: ${props => props.theme.pallete.paper};
+  color: ${props => props.theme.pallete.text.primary};
+  border-radius: 10px;
 `;
 
-export default Card;
+export default CardBody;
