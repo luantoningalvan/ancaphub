825c8dbdabe313f006ea02073b6a9e29c6c52138
AncapHub <55035518+ancaphub@users.noreply.github.com>
2020-03-19 00:14:28 -0300

    Adiciona componente CardFooter

---
+++ client/src/components/ui/CardFooter.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from "styled-components"
+import { Link } from "react-router-dom";
+
+const CardFooter = styled(Link)`
+    width: 100%;
+    text-align:center;
+    padding:15px;
+    color: white;
+    text-decoration:none;
+    border-top:1px solid #2f3749;
+    transition:0.3s;
+    &:hover {   
+        background: rgba(0,0,0,0.1)
+    }
+`;
+
+export default props => (
+    <CardFooter to={props.link}>
+        {props.label}
+    </CardFooter>
+)
