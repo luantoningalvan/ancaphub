6f1c172f23284f75e1d3c70613e8ac16a500a0d8
= <=>
2020-04-03 18:00:29 -0300

    Inclusão de variável de ambiente na url de chamada da API

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: process.env.REACT_APP_API_URL 
+    ? process.env.REACT_APP_API_URL + "/api"
+    : 'http://localhost:3333/api',
 });
