d7474c5cb3399d7db363078dadc6b6506f111d13
= <=>
2020-04-04 15:12:04 -0300

    Cria componente de input que integra campos de formulário ao unform

---
+++ client/src/components/form/Input.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useRef } from "react";
+import { useField } from "@unform/core";
+import TextField from "../ui/TextField";
+
+export default function Input({ name, ...rest }) {
+  const inputRef = useRef(null);
+  const { fieldName, defaultValue, registerField, error } = useField(name);
+  
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: "value",
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <>
+      <TextField
+        ref={inputRef}
+        defaultValue={defaultValue}
+        hasError={error}
+        {...rest}
+      />
+      { error && <span style={{color: "#f93c3c", fontSize: '0.9em', marginTop: 8}}>{error}</span> }
+    </>
+  );
+}
