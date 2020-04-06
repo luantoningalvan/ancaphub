fa64b900d9846ce9739d5f904dd570bd20b3ad76
= <=>
2020-04-05 23:08:09 -0300

    Cria componente para exibição de alertas e adiciona paleta de cores para os mesmos

--- client/src/components/template/Provider.js
+++ client/src/components/template/Provider.js
@@ -8,6 +8,12 @@ const themes = {
       primary: '#1141ad',
       secondary: '#e0b30d',
       background: '#161b28',
+      alert: {
+        info: "#0099cc",
+        success: "#007e33",
+        error: "#cc0000",
+        warning: "#f80"
+      },
       paper: '#21283b',
       paperDark: '#1d2333',
       border: '#2f3749',
@@ -23,6 +29,12 @@ const themes = {
       primary: '#1141ad',
       secondary: '#e0b30d',
       background: '#f5f5f5',
+      alert: {
+        info: "#33b5e5",
+        success: "#00c851",
+        error: "#f44",
+        warning: "#fb3"
+      },
       paper: '#fff',
       paperDark: '#fff',
       border: '#ececec',
