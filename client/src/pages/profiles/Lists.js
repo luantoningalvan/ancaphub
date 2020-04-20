f05ce42008041bf2a5ea4ddd62f1a9248377b48d
= <=>
2020-04-20 13:14:50 -0300

    Cria Card para exibição de lista e página no perfil

--- client/src/pages/profiles/Lists.js
+++ client/src/pages/profiles/Lists.js
@@ -1,5 +1,36 @@
-import React from 'react';
+import React from "react";
+import ListCard from "../../components/lists/ListCard";
+import GridContainer from "../../components/ui/GridContainer";
+import GridItem from "../../components/ui/GridItem";
 
-const Collection = () => <p>Collection</p>;
+const Lists = () => {
+  const lists = [
+  {
+    title: "Pretendo ler",
+    itemsCount: 3,
+    cover: "https://images-na.ssl-images-amazon.com/images/I/9140LT91hYL.jpg"
+  },
+  {
+    title: "Lidos",
+    itemsCount: 12,
+    cover: "https://cdn.mises.org.br/images/articles/b6f1001c42844f29b2832c713c13c5bc.jpg"
+  },
+  {
+    title: "Iniciantes",
+    itemsCount: 8,
+    cover: "https://images-na.ssl-images-amazon.com/images/I/71cA1CzENgL.jpg"
+  }
+];
 
-export default Collection;
+  return (
+      <GridContainer spacing={2}>
+        {lists.map((list) => (
+          <GridItem xs={4}>
+            <ListCard list={list} />
+          </GridItem>
+        ))}
+      </GridContainer>
+  );
+};
+
+export default Lists;
