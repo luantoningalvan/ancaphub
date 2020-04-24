769045030dceee027aae6199e9f37ff58875658f
= <=>
2020-04-24 01:02:08 -0300

    Modifica componentes p/ criação de abas fornecendo maior versatilidade

--- client/src/components/ui/Tab.js
+++ client/src/components/ui/Tab.js
@@ -16,9 +16,16 @@ const TabStyle = styled.li`
 
   > a {
     display: block;
-    color: white;
+    color: ${props => props.theme.palette.text.primary};
     text-decoration: none;
-    padding: 16px;
+    padding:  0px 16px;
+    height:100%;
+    display:flex;
+    align-items:center;
+
+    svg {
+      fill: ${props => props.theme.palette.text.primary}
+    }
   }
 `;
 
