9f9de9f90e7e385c2c63a215bc5743cca9751ffc
= <=>
2020-04-30 11:49:48 -0300

    Remove Dashboard

--- dashboard/src/components/template/titleComponent.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Helmet from 'react-helmet';
-
-export default ({ title }) => (
-  <Helmet>
-    <title>{title ? `${title} - AncapHub` : 'Painel Administrativo - AncapHub'}</title>
-  </Helmet>
-)
