166e7f8c5b8ff052a287f819b81ec14dbeee8ee1
Vini Franco <email@vinifranco.com.br>
2020-05-17 20:53:00 -0300

    fix(i18n): signup form

--- client/src/components/auth/SignupForm.js
+++ client/src/components/auth/SignupForm.js
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react';
 import { Form } from '@unform/web';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 import * as Yup from 'yup';
 import { useDispatch } from 'react-redux';
 import Button from '../ui/Button';
@@ -12,31 +12,32 @@ import GridItem from '../ui/GridItem';
 export default () => {
   const dispatch = useDispatch();
   const signupFormRef = useRef(null);
+  const { formatMessage } = useIntl();
 
   async function handleSubmit(data) {
     try {
       const schema = Yup.object().shape({
         name: Yup.string()
-          .min(3, <FormattedMessage id="account.settings.validation.usernameShort" />)
-          .max(30, <FormattedMessage id="account.settings.validation.usernameLong" />)
-          .required(<FormattedMessage id="account.settings.validation.usernameRequired" />),
+          .min(3, formatMessage({ id: 'account.settings.validation.usernameShort' }))
+          .max(30, formatMessage({ id: 'account.settings.validation.usernameLong' }))
+          .required(formatMessage({ id: 'account.settings.validation.usernameRequired' })),
         username: Yup.string()
-          .min(3, <FormattedMessage id="account.settings.validation.usernameShort" />)
-          .max(20, <FormattedMessage id="account.settings.validation.usernameLong" />)
-          .matches(/^[a-zA-Z0-9_]+$/, <FormattedMessage id="account.settings.validation.regex" />)
-          .required(<FormattedMessage id="account.settings.validation.usernameRequired" />),
+          .min(3, formatMessage({ id: 'account.settings.validation.usernameShort' }))
+          .max(20, formatMessage({ id: 'account.settings.validation.usernameLong' }))
+          .matches(/^[a-zA-Z0-9_]+$/, formatMessage({ id: 'account.settings.validation.regex' }))
+          .required(formatMessage({ id: 'account.settings.validation.usernameRequired' })),
         email: Yup.string()
-          .email(<FormattedMessage id="account.settings.validation.emailInvalid" />)
-          .required(<FormattedMessage id="account.settings.validation.emailRequired" />),
+          .email(formatMessage({ id: 'account.settings.validation.emailInvalid' }))
+          .required(formatMessage({ id: 'account.settings.validation.emailRequired' })),
         password: Yup.string()
-          .required(<FormattedMessage id="account.settings.validation.currentPasswordRequired" />)
-          .min(6, <FormattedMessage id="account.settings.validation.minPasswordLength" />),
+          .required(formatMessage({ id: 'account.settings.validation.currentPasswordRequired' }))
+          .min(6, formatMessage({ id: 'account.settings.validation.minPasswordLength' })),
         confirmPassword: Yup.string()
-          .required(<FormattedMessage id="account.settings.validation.confirmPasswordRequired" />)
-          .oneOf([Yup.ref('password'), null], <FormattedMessage id="account.settings.validation.passwordMismatch" />),
+          .required(formatMessage({ id: 'account.settings.validation.confirmPasswordRequired' }))
+          .oneOf([Yup.ref('password'), null], formatMessage({ id: 'account.settings.validation.passwordMismatch' })),
         code: Yup.string()
-          .required(<FormattedMessage id="alpha.validation.inviteRequired" />)
-          .length(20, <FormattedMessage id="alpha.validation.inviteLength" />),
+          .required(formatMessage({ id: 'alpha.validation.inviteRequired' }))
+          .length(20, formatMessage({ id: 'alpha.validation.inviteLength' })),
       });
       await schema.validate(data, {
         abortEarly: false,
