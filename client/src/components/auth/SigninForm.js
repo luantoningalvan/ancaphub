d7beb3d7673d1ca6d625ac39d0aa80dac987b078
Vini Franco <email@vinifranco.com.br>
2020-05-17 20:44:39 -0300

    fix(i18n): add intl to signin form

--- client/src/components/auth/SigninForm.js
+++ client/src/components/auth/SigninForm.js
@@ -1,24 +1,25 @@
-import React, { useRef } from "react";
-import Input from "../form/Input";
-import { Form } from "@unform/web";
-import Button from "../ui/Button";
-import * as Yup from "yup";
-import { useDispatch } from "react-redux";
-import { authUserRequest } from "../../actions/auth";
-import GridContainer from "../ui/GridContainer";
-import GridItem from "../ui/GridItem";
+import React, { useRef } from 'react';
+import { FormattedMessage, useIntl } from 'react-intl';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import { useDispatch } from 'react-redux';
+import Button from '../ui/Button';
+import Input from '../form/Input';
+import { authUserRequest } from '../../actions/auth';
+import GridContainer from '../ui/GridContainer';
+import GridItem from '../ui/GridItem';
 
 export default () => {
   const dispatch = useDispatch();
   const signupFormRef = useRef(null);
-
+  const { formatMessage } = useIntl();
   async function handleSubmit(data) {
     try {
       const schema = Yup.object().shape({
         email: Yup.string()
-          .email("E-mail inválido")
-          .required("O campo e-mail é obrigatório!"),
-        password: Yup.string().required("O campo senha é obrigatório!"),
+          .email(formatMessage({ id: 'account.settings.validation.emailInvalid' }))
+          .required(formatMessage({ id: 'account.settings.validation.emailRequired' })),
+        password: Yup.string().required({ id: 'account.settings.validation.passwordRequired' }),
       });
       await schema.validate(data, {
         abortEarly: false,
@@ -39,25 +40,33 @@ export default () => {
     <Form onSubmit={handleSubmit} ref={signupFormRef}>
       <GridContainer spacing={1}>
         <GridItem xs={12}>
-          <Input
-            type="email"
-            placeholder="E-mail"
-            name="email"
-            autoComplete="email"
-          />
+          <FormattedMessage id="common.email">
+            {(msg) => (
+              <Input
+                type="email"
+                placeholder={msg}
+                name="email"
+                autoComplete="email"
+              />
+            )}
+          </FormattedMessage>
         </GridItem>
 
         <GridItem xs={12}>
-          <Input
-            type="password"
-            placeholder="Senha"
-            name="password"
-            autoComplete="password"
-          />
+          <FormattedMessage id="common.password">
+            {(msg) => (
+              <Input
+                type="password"
+                placeholder={msg}
+                name="password"
+                autoComplete="password"
+              />
+            )}
+          </FormattedMessage>
         </GridItem>
         <GridItem xs={12}>
-          <Button type="submit" color="secondary" style={{ width: "100%" }}>
-            Entrar
+          <Button type="submit" color="secondary" style={{ width: '100%' }}>
+            <FormattedMessage id="common.login" description="Login button" />
           </Button>
         </GridItem>
       </GridContainer>
