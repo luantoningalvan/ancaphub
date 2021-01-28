3301c3e5d3ace8ed33d211dc18c58faee6a40b89
Luan Tonin Galvan <ltoningalvan@gmail.com>
2021-01-27 23:16:26 -0300

    Cria migração de UserSettings

---
+++ src/shared/infra/typeorm/migrations/1611799478762-CreateUserSettings.ts
@@ -0,0 +1,52 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUserSettingS1611799478762 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'user_settings',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'user',
+            type: 'uuid',
+          },
+          {
+            name: 'geolocation',
+            type: 'boolean',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'User',
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            columnNames: ['user'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('user_settings');
+  }
+}
